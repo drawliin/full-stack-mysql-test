f761a1f2976d13dec41573e91086d602b75ca155
Houssam Eddine HAMOUICH <itshoussameddine@gmail.com>
2025-02-16 00:10:56 +0100

    remove Dockerfiles and configuration for backend, database, frontend, and Nginx

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,7 @@ const { Sequelize } = require('sequelize');
 const sequelize = new Sequelize(
   process.env.DB_NAME || 'test', // Database name
   process.env.DB_USER || 'root', // Username
-  process.env.DB_PASS || 'password', // Password
+  process.env.DB_PASS || 'root', // Password
   {
     host: process.env.DB_HOST || 'localhost', // Database host
     dialect: 'mysql',
