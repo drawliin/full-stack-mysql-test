3724a40699f2470e78920dcccb77de8ba7a64f6b
drawlin <itshoussameddine@gmail.com>
2025-02-19 17:18:51 +0100

    refactor: update database password and integrate dotenv for environment variables

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,7 @@ const { Sequelize } = require('sequelize');
 const sequelize = new Sequelize(
   process.env.DB_NAME || 'test', // Database name
   process.env.DB_USER || 'root', // Username
-  process.env.DB_PASS || 'root', // Password
+  process.env.DB_PASS || 'password', // Password
   {
     host: process.env.DB_HOST || 'localhost', // Database host
     dialect: 'mysql',
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,8 @@ function App() {
   const [users, setUsers] = useState([]);
   const [newUser, setNewUser] = useState({ name: '', email: '' }); // State for the new user
   const [loading, setLoading] = useState(true);
-  const API_URL = process.env.REACT_APP_API_URL || 4002;
+  const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:4002/api';
+  console.log(API_URL)
 
   useEffect(() => {
     fetchUsers();
